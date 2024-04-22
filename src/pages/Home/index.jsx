d84ebfff4c8260bbe95a8eac2700a8857db11dff
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-04-22 19:50:52 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+      <>
+        <h1>Bem vindo a pagina HOME!</h1>
+      </>
+    )
+  }
---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,7 @@
+export const Sobre = () => {
+    return (
+      <>
+        <h1>Pagina Sobre com React-Router-Dom</h1>
+      </>
+    )
+  }
